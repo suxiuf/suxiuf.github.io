e2d5c224522da760747039cdafb8765e18c3d66d
suxiuf <47658925+suxiuf@users.noreply.github.com>
2024-07-02 16:29:49 +0800

    test

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -7,9 +7,6 @@ export default navbar([
     text: "oscp",
     icon: "book",
     prefix: "/oscp/",
-    children: [],
-  },
-  { text: "pmp", icon: "pen-to-square", link: "/posts/pmp/index.md" },
-  { text: "打靶记录", icon: "pen-to-square", link: "/posts/range/index.md" },
-
+    children: "structure",
+  }
 ]);
